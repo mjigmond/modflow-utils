c16ccecf088e5b89341caf5eaff40c9ec1dd53f5
Marius Jigmond <mjigmond@gmail.com>
2019-04-23 09:48:36 -0600

    Added structured CBB parsing function

--- binParsers.py
+++ binParsers.py
@@ -9,14 +9,14 @@ import numpy as np
 import os
 
 def parseHDS(fn=None, nlay=0, nrow=0, ncol=0) -> tuple:
-    '''This function will parse a binary *.hds file
+    '''This function will parse a structured binary *.hds file
     and will return a tuple with two items.
     The first item is dictionary containing the head arrays as values and 
     stress periods/time steps tuples as keys.
     The second item is an array of total times matching the length of the dictionary.
     '''
     if fn is None or nlay == 0 or nrow == 0 or ncol == 0:
-        return {}
+        return ({}, np.empty(0))
     fsize = os.path.getsize(fn)
     ofst = 0
     hds = {}
@@ -27,7 +27,7 @@ def parseHDS(fn=None, nlay=0, nrow=0, ncol=0) -> tuple:
                 ,('kper', 'i4')
                 ,('pertim', 'f4')
                 ,('totim', 'f4')
-                ,('desc', 'S16')
+                ,('text', 'S16')
                 ,('ncol', 'i4')
                 ,('nrow', 'i4')
                 ,('ilay', 'i4')
@@ -40,3 +40,33 @@ def parseHDS(fn=None, nlay=0, nrow=0, ncol=0) -> tuple:
         hds[kper, kstp] = arr['data'].copy()
         totim.append(arr['totim'][0])
     return hds, np.asarray(totim)
+
+def parseCBB(fn=None, nlay=0, nrow=0, ncol=0) -> dict:
+    '''This function will parse a structured binary *.cbb file
+    and will return a dictionary containing the flux arrays as values and 
+    stress periods/time steps tuples as keys. Budget terms are stripped of
+    white space at the beginning and at the end.
+    '''
+    if fn is None or nlay == 0 or nrow == 0 or ncol == 0:
+        return {}
+    fsize = os.path.getsize(fn)
+    ofst = 0
+    cbb = {}
+    while ofst < fsize:
+        dt = np.dtype([
+                ('kstp', 'i4')
+                ,('kper', 'i4')
+                ,('text', 'S16')
+                ,('ncol', 'i4')
+                ,('nrow', 'i4')
+                ,('nlay', 'i4')
+                ,('data', 'f4', (nlay, nrow, ncol))
+        ])
+        arr = np.memmap(fn, mode='r', dtype=dt, offset=ofst, shape=1)[0]
+        ofst += dt.itemsize
+        text = arr['text'].decode().strip()
+        kper = arr['kper']
+        kstp = arr['kstp']
+        cbb.setdefault(text, {})
+        cbb[text][kper, kstp] = arr['data'].copy()
+    return cbb
