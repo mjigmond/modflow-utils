b63999fd33bbee076162d680fa2576db5949c83a
Marius Jigmond <mjigmond@gmail.com>
2019-11-07 19:32:01 -0700

    Some pydoc styling cleanup

--- binParsers.py
+++ binParsers.py
@@ -9,12 +9,14 @@ import numpy as np
 import os
 
 def parseHDS(fn=None, nlay=0, nrow=0, ncol=0) -> tuple:
-    '''This function will parse a structured binary *.hds file
+    """
+    This function will parse a structured binary *.hds file
     and will return a tuple with two items.
-    The first item is dictionary containing the head arrays as values and 
+    The first item is dictionary containing the head arrays as values and
     stress periods/time steps tuples as keys.
-    The second item is an array of total times matching the length of the dictionary.
-    '''
+    The second item is an array of total times matching
+    the length of the dictionary.
+    """
     if fn is None or nlay == 0 or nrow == 0 or ncol == 0:
         return ({}, np.empty(0))
     fsize = os.path.getsize(fn)
@@ -23,15 +25,15 @@ def parseHDS(fn=None, nlay=0, nrow=0, ncol=0) -> tuple:
     totim = []
     while ofst < fsize:
         dt = np.dtype([
-                ('kstp', 'i4')
-                ,('kper', 'i4')
-                ,('pertim', 'f4')
-                ,('totim', 'f4')
-                ,('text', 'S16')
-                ,('ncol', 'i4')
-                ,('nrow', 'i4')
-                ,('ilay', 'i4')
-                ,('data', 'f4', (nrow, ncol))
+                ('kstp', 'i4'),
+                ('kper', 'i4'),
+                ('pertim', 'f4'),
+                ('totim', 'f4'),
+                ('text', 'S16'),
+                ('ncol', 'i4'),
+                ('nrow', 'i4'),
+                ('ilay', 'i4'),
+                ('data', 'f4', (nrow, ncol))
         ])
         arr = np.memmap(fn, mode='r', dtype=dt, offset=ofst, shape=nlay)
         ofst += dt.itemsize * nlay
@@ -42,11 +44,12 @@ def parseHDS(fn=None, nlay=0, nrow=0, ncol=0) -> tuple:
     return hds, np.asarray(totim)
 
 def parseCBB(fn=None, nlay=0, nrow=0, ncol=0) -> dict:
-    '''This function will parse a structured binary *.cbb file
-    and will return a dictionary containing the flux arrays as values and 
+    """
+    This function will parse a structured binary *.cbb file
+    and will return a dictionary containing the flux arrays as values and
     stress periods/time steps tuples as keys. Budget terms are stripped of
     white space at the beginning and at the end.
-    '''
+    """
     if fn is None or nlay == 0 or nrow == 0 or ncol == 0:
         return {}
     fsize = os.path.getsize(fn)
@@ -54,13 +57,13 @@ def parseCBB(fn=None, nlay=0, nrow=0, ncol=0) -> dict:
     cbb = {}
     while ofst < fsize:
         dt = np.dtype([
-                ('kstp', 'i4')
-                ,('kper', 'i4')
-                ,('text', 'S16')
-                ,('ncol', 'i4')
-                ,('nrow', 'i4')
-                ,('nlay', 'i4')
-                ,('data', 'f4', (nlay, nrow, ncol))
+                ('kstp', 'i4'),
+                ('kper', 'i4'),
+                ('text', 'S16'),
+                ('ncol', 'i4'),
+                ('nrow', 'i4'),
+                ('nlay', 'i4'),
+                ('data', 'f4', (nlay, nrow, ncol))
         ])
         arr = np.memmap(fn, mode='r', dtype=dt, offset=ofst, shape=1)[0]
         ofst += dt.itemsize
